2e4d1ce37ece85523b6374b20895edbea68e45aa
rpascal <ryanpascal99@yahoo.com>
2016-11-19 14:05:29 -0500

    Login

---
+++ src/app/components/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import {LoginRoutingModule} from './login-routing.module';
+import {SharedModule} from '../../shared/shared.module';
+
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [
+    LoginRoutingModule,
+    SharedModule
+
+  ],
+  declarations: [LoginComponent]
+})
+export class LoginModule { }
